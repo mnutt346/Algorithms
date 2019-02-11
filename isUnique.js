bd443eb50d294132d7277733e02e7ee56dd2f05c
Michael Nutt <mnutt346@gmail.com>
2019-02-11 15:01:48 -0600

    Solved for O(n log(n)) run time.

--- isUnique.js
+++ isUnique.js
@@ -1,10 +1,28 @@
-// Naive solution
+// ----- Naive solution -----
+// const isUnique = string => {
+//   let counter = {};
+//   for (let char of string) {
+//     if (counter[char]) {
+//         return false;
+//     } else {
+//         counter[char] = true;
+//     }
+//   }
+//   return true;
+// };
+// O(n) run time ::: O(n) space
+
 const isUnique = string => {
-  let counter = {};
-  for (let char of string) {
-    counter[char] ? false : (counter[char] = true);
+  let sorted = string.split("").sort();
+  let pointer1 = 0,
+    pointer2 = 1;
+  while (pointer2 < sorted.length) {
+    if (sorted[pointer1] === sorted[pointer2]) return false;
+    pointer1++;
+    pointer2++;
   }
   return true;
 };
 
-isUnique("banana");
+console.log(isUnique("abcdefghijklmnopqrstuvwxyz"));
+console.log(isUnique("popcorn"));
