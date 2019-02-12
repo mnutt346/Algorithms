f1ce32607804663906ea6a6e0da005ca06ab2a8e
Michael Nutt <mnutt346@gmail.com>
2019-02-12 14:07:13 -0600

    Correction.

--- isUnique.js
+++ isUnique.js
@@ -16,13 +16,15 @@ const isUnique = string => {
   let sorted = string.split("").sort();
   let pointer1 = 0,
     pointer2 = 1;
+  console.log(sorted);
   while (pointer2 < sorted.length) {
     if (sorted[pointer1] === sorted[pointer2]) return false;
-    pointer1 += 2;
-    pointer2 += 2;
+    pointer1++;
+    pointer2++;
   }
   return true;
 };
 
 console.log(isUnique("abcdefghijklmnopqrstuvwxyz"));
 console.log(isUnique("popcorn"));
+console.log(isUnique("barbeque"));
