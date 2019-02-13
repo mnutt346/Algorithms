407e2122d058f973f8d50622ab4ac211ae38f760
Michael Nutt <mnutt346@gmail.com>
2019-02-13 15:18:05 -0600

    Complete reverseStringInPlace.

---
+++ reverseStringInPlace.js
@@ -0,0 +1,23 @@
+/*
+Write a function that takes an array of characters and reverses the letters in place.
+*/
+
+const reverseString = stringArray => {
+  let startPointer = 0,
+    endPointer = stringArray.length - 1;
+
+  while (startPointer < endPointer) {
+    [stringArray[startPointer], stringArray[endPointer]] = [
+      stringArray[endPointer],
+      stringArray[startPointer]
+    ];
+    startPointer++;
+    endPointer--;
+  }
+  return stringArray;
+};
+
+// O(log n)
+
+console.log(reverseString(["c", "a", "r"]));
+console.log(reverseString(["b", "o", "a", "t"]));
