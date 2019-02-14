c88a1be26bbc5e1ad989aa620340edc49e3a4c1b
Michael Nutt <mnutt346@gmail.com>
2019-02-14 14:45:30 -0600

    Remove redundancy from validAnagrams.

--- validAnagrams.js
+++ validAnagrams.js
@@ -1,19 +1,15 @@
 function validAnagram(string1, string2) {
   if (string1.length !== string2.length) return false;
 
-  let string1Counter = {};
-  let string2Counter = {};
+  let counter = {};
 
   for (let char of string1) {
-    string1Counter[char] = (string1Counter[char] || 0) + 1;
-  }
-  for (let char of string2) {
-    string2Counter[char] = (string2Counter[char] || 0) + 1;
+    counter[char] ? (counter[char] += 1) : (counter[char] = 1);
   }
 
-  for (let key in string1Counter) {
-    if (!(key in string2Counter)) return false;
-    if (string1Counter[key] !== string2Counter[key]) return false;
+  for (let char of string2) {
+    if (!counter[char]) return false;
+    else counter[char] -= 1;
   }
 
   return true;
