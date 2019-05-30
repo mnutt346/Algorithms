b45ed21046ac1eaf496ec0bf7acdcfbbd83d4543
Michael Nutt <mnutt346@gmail.com>
2019-05-30 17:22:43 -0500

    Optimize findLongestSubstring.

--- findLongestSubstring.js
+++ findLongestSubstring.js
@@ -1,22 +1,39 @@
+// const findLongestSubstring = str => {
+//   if (!str.length) return 0;
+
+//   let start = 0,
+//     end = 1,
+//     longest = 0,
+//     subStr = str[start];
+
+//   while (end < str.length) {
+//     if (subStr.includes(str[end])) {
+//       longest = longest < subStr.length ? subStr.length : longest;
+//       start = str.indexOf(str[end], start) + 1;
+//       end = start + 1;
+//       subStr = str[start];
+//     } else {
+//       subStr += str[end];
+//       longest = longest < subStr.length ? subStr.length : longest;
+//       end++;
+//     }
+//   }
+//   return longest;
+// };
+
 const findLongestSubstring = str => {
-  if (!str.length) return 0;
-
   let start = 0,
-    end = 1,
     longest = 0,
-    subStr = str[start];
+    tracker = {};
 
-  while (end < str.length) {
-    if (subStr.includes(str[end])) {
-      longest = longest < subStr.length ? subStr.length : longest;
-      start = str.indexOf(str[end], start) + 1;
-      end = start + 1;
-      subStr = str[start];
-    } else {
-      subStr += str[end];
-      longest = longest < subStr.length ? subStr.length : longest;
-      end++;
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i];
+    if (tracker[char]) {
+      start = Math.max(start, tracker[char]);
     }
+
+    longest = Math.max(longest, i - start + 1);
+    tracker[char] = i + 1;
   }
   return longest;
 };
