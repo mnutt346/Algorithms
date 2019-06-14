0505f9f0420e073e4873cf6a27e98540803211d0
Michael Nutt <mnutt346@gmail.com>
2019-06-14 13:06:55 -0500

    Create linked list and add push and pop methods.

---
+++ linkedList.js
@@ -0,0 +1,58 @@
+class LinkedList {
+  constructor() {
+    this.length = 0;
+    this.head = null;
+    this.tail = null;
+  }
+
+  push(val) {
+    let newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  pop() {
+    if (!this.head) return undefined;
+
+    let current = this.head,
+      newTail = current;
+
+    while (current.next) {
+      newTail = current;
+      current = current.next;
+    }
+    newTail.next = null;
+    this.tail = newTail;
+    this.length--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return current;
+  }
+}
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+let list = new LinkedList();
+
+list.push(1);
+console.log(list);
+list.push(2);
+console.log(list);
+list.push(3);
+list.push(4);
+list.pop();
+console.log(list);
