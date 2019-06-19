94e8cda218ccf3b4045878d05084141b7e6e28ae
Michael Nutt <mnutt346@gmail.com>
2019-06-19 14:52:46 -0500

    Create skeleton for doublyLinkedList.

---
+++ doublyLinkedList.js
@@ -0,0 +1,15 @@
+class Node {
+  constructor(val) {
+    this.value = val;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+class DoublyLinkedList {
+  constructor() {
+    this.length = 0;
+    this.head = null;
+    this.tail = null;
+  }
+}
